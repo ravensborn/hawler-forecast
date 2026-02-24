67ae91f00d816c6d29b1a2c22fde3e45ffb22058
yad <yad.hoshyar@gmail.com>
2026-02-24 13:08:54 +0300

    Add language query parameter to Alert, MapPin, and SensorDeviceGroup controllers for translatable fields

--- app/Http/Controllers/Api/MapPinController.php
+++ app/Http/Controllers/Api/MapPinController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MapPinResource;
 use App\Models\MapPin;
+use Dedoc\Scramble\Attributes\QueryParameter;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class MapPinController extends Controller
 {
+    #[QueryParameter('language', description: 'The language code for translatable fields.', type: 'string', example: 'en')]
     public function index(): AnonymousResourceCollection
     {
         return MapPinResource::collection(
